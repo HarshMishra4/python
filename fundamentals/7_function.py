1a7427bb51dcbb47db321d8bc5778479e5f9c76d
Mishra Harsh Sanjay <34826453+HarshMishra4@users.noreply.github.com>
2022-05-13 11:17:59 +0530

    Add files via upload

---
+++ fundamentals/7_function.py
@@ -0,0 +1,25 @@
+# ================================
+# ======[ Basic approach ]========
+# ================================
+a = 5
+b = 8
+if (a + b) > 10:
+    print("Some random message")
+
+a = 89
+b = 2
+print(a + b)
+
+# ================================
+# =====[ By using function ]======
+# ================================
+
+# Function Defination
+def add(a, b):
+    total = a + b
+    return total
+
+
+# Funtion call
+result = add(5, 8)
+print(result)
