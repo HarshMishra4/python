b6caf9e5554deaf335923053cc19699b7568eab2
Mishra Harsh Sanjay <34826453+HarshMishra4@users.noreply.github.com>
2022-06-06 18:32:00 +0530

    Add files via upload

---
+++ fundamentals/14_lambda_function.py
@@ -0,0 +1,11 @@
+# [ Lambda Function declaration with single argument ]
+# The job of this function is find square of any
+# given number
+square = lambda x: x * x
+# Funtion call
+print("Square : " + str(square(8)))
+
+"""Lambda Function declaration with multiple arguments"""
+add = lambda a, b: a + b
+# Funtion call
+print("Addition : " + str(add(3, 8)))
